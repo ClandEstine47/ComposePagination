78d8f34e46dede9c6f3273148328fe0251acb269
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2023-09-29 17:18:42 +0545

    topApp bar added and statusBar color changed

--- app/src/main/java/com/plcoding/composepaging3caching/ui/theme/Theme.kt
+++ app/src/main/java/com/plcoding/composepaging3caching/ui/theme/Theme.kt
@@ -1,10 +1,15 @@
 package com.plcoding.composepaging3caching.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -44,4 +49,16 @@ fun ComposePaging3CachingTheme(
         shapes = Shapes,
         content = content
     )
+
+    // This part helps you set the statusbar color
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colors.background.toArgb()
+
+            WindowCompat.getInsetsController(window, view)
+                .isAppearanceLightStatusBars = !darkTheme
+        }
+    }
 }
